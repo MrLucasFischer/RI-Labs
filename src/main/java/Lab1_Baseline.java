620f130c0af41bafaabf1bebd3e7e73d4c029fa1
MrLucasFischer <lucasfischerpt@gmail.com>
2018-10-24 15:29:18 +0100

    Updated Lab2

--- src/main/java/Lab1_Baseline.java
+++ src/main/java/Lab1_Baseline.java
@@ -1,5 +1,3 @@
-package org.novasearch.tutorials.labs2018;
-
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.*;
@@ -40,7 +38,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Lab1_Baseline {
     String indexPath = "./index";
-    String docPath = "./data/Answers.csv";
+    String docPath = "./eval/Answers.csv";
 
     boolean create = true;
 
@@ -59,184 +57,180 @@ public class Lab1_Baseline {
         baseline.openIndex(analyzer, similarity);   //creates an in-disk index
         baseline.indexDocuments();  //parses the documents and puts them in them index
         baseline.close();
-        
+
         // Search the index
         baseline.indexSearch(analyzer, similarity); //Search the index
     }
 
-	public void openIndex(Analyzer analyzer, Similarity similarity) {
-		try {
-			// ====================================================
-			// Configure the index to be created/opened
-			//
-			// IndexWriterConfig has many options to be set if needed.
-			//
-			// Example: for better indexing performance, if you
-			// are indexing many documents, increase the RAM
-			// buffer. But if you do this, increase the max heap
-			// size to the JVM (eg add -Xmx512m or -Xmx1g):
-			//
-			// iwc.setRAMBufferSizeMB(256.0);
-			IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
-			iwc.setSimilarity(similarity);
-			if (create) {
-				// Create a new index, removing any
-				// previously indexed documents:
-				iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-			} else {
-				// Add new documents to an existing index:
-				iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
-			}
+    public void openIndex(Analyzer analyzer, Similarity similarity) {
+        try {
+            // ====================================================
+            // Configure the index to be created/opened
+            //
+            // IndexWriterConfig has many options to be set if needed.
+            //
+            // Example: for better indexing performance, if you
+            // are indexing many documents, increase the RAM
+            // buffer. But if you do this, increase the max heap
+            // size to the JVM (eg add -Xmx512m or -Xmx1g):
+            //
+            // iwc.setRAMBufferSizeMB(256.0);
+            IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+            iwc.setSimilarity(similarity);
+            if (create) {
+                // Create a new index, removing any
+                // previously indexed documents:
+                iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+            } else {
+                // Add new documents to an existing index:
+                iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+            }
 
-			// ====================================================
-			// Open/create the index in the specified location
-			Directory dir = FSDirectory.open(Paths.get(indexPath));
-			idx = new IndexWriter(dir, iwc);
+            // ====================================================
+            // Open/create the index in the specified location
+            Directory dir = FSDirectory.open(Paths.get(indexPath));
+            idx = new IndexWriter(dir, iwc);
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	public void indexDocuments() {
-		if (idx == null)
-			return;
+    public void indexDocuments() {
+        if (idx == null)
+            return;
 
-		// ====================================================
-		// Parse the Answers data
-		try (BufferedReader br = new BufferedReader(new FileReader(docPath))) {
-			StringBuilder sb = new StringBuilder();
-			String line = br.readLine(); // The first line is dummy
-			line = br.readLine();
+        // ====================================================
+        // Parse the Answers data
+        try (BufferedReader br = new BufferedReader(new FileReader(docPath))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine(); // The first line is dummy
+            line = br.readLine();
 
-			// ====================================================
-			// Read documents
-			while (line != null) {
-				int i = line.length();
+            // ====================================================
+            // Read documents
+            while (line != null) {
+                int i = line.length();
 
-				// Search for the end of document delimiter
-				if (i != 0)
-					sb.append(line);
-				sb.append(System.lineSeparator());
-				if (((i >= 2) && (line.charAt(i - 1) == '"') && (line.charAt(i - 2) != '"'))
-						|| ((i == 1) && (line.charAt(i - 1) == '"'))) {
-					// Index the document
-					indexDoc(sb.toString());
+                // Search for the end of document delimiter
+                if (i != 0)
+                    sb.append(line);
+                sb.append(System.lineSeparator());
+                if (((i >= 2) && (line.charAt(i - 1) == '"') && (line.charAt(i - 2) != '"'))
+                        || ((i == 1) && (line.charAt(i - 1) == '"'))) {
+                    // Index the document
+                    indexDoc(sb.toString());
 
-					// Start a new document
-					sb = new StringBuilder();
-				}
-				line = br.readLine();
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+                    // Start a new document
+                    sb = new StringBuilder();
+                }
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	public void indexDoc(String rawDocument) {
+    public void indexDoc(String rawDocument) {
 
-		Document doc = new Document();
+        Document doc = new Document();
 
-		// ====================================================
-		// Each document is organized as:
-		// Id,OwnerUserId,CreationDate,ParentId,Score,Body
-		Integer AnswerId = 0;
-		try {
+        // ====================================================
+        // Each document is organized as:
+        // Id,OwnerUserId,CreationDate,ParentId,Score,Body
+        Integer AnswerId = 0;
+        try {
 
-			// Extract field Id
-			Integer start = 0;
-			Integer end = rawDocument.indexOf(',');
-			String aux = rawDocument.substring(start, end);
-			AnswerId = Integer.decode(aux);
+            // Extract field Id
+            Integer start = 0;
+            Integer end = rawDocument.indexOf(',');
+            String aux = rawDocument.substring(start, end);
+            AnswerId = Integer.decode(aux);
 
-			// Index _and_ store the AnswerId field
-			doc.add(new IntPoint("AnswerId", AnswerId));
-			doc.add(new StoredField("AnswerId", AnswerId));
+            // Index _and_ store the AnswerId field
+            doc.add(new IntPoint("AnswerId", AnswerId));
+            doc.add(new StoredField("AnswerId", AnswerId));
 
-			// Extract field OwnerUserId
-			start = end + 1;
-			end = rawDocument.indexOf(',', start);
-			aux = rawDocument.substring(start, end);
-			Integer OwnerUserId = Integer.decode(aux);
-			doc.add(new IntPoint("OwnerUserId", OwnerUserId));
+            // Extract field OwnerUserId
+            start = end + 1;
+            end = rawDocument.indexOf(',', start);
+            aux = rawDocument.substring(start, end);
+            Integer OwnerUserId = Integer.decode(aux);
+            doc.add(new IntPoint("OwnerUserId", OwnerUserId));
 
-			// Extract field CreationDate
-			try {
-				start = end + 1;
-				end = rawDocument.indexOf(',', start);
-				aux = rawDocument.substring(start, end);
-				Date creationDate;
-				creationDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(aux);
-				doc.add(new LongPoint("CreationDate", creationDate.getTime()));
-				
-				// Add decay weight field
-				//System.out.println("??????????????????????????????????????????????");
-				LocalDate docDate = creationDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-				
-				Date docDate1 = Date.from(docDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-				Date now = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
-				long diff = now.getTime() - docDate1.getTime();
-				int totalDaysElapsed = (int) TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-				
-				// Calculation of total elapsed number of days ends here
-				System.out.println(totalDaysElapsed);
-				//double decayFactor = 1.0 / Math.pow(2.0, totalDaysElapsed);
-				double decayFactor = 1.0 / Math.pow((1.0 + (1.0/(float)totalDaysElapsed)), (float)totalDaysElapsed);
-				
-				// JOANA DECAY
-				doc.add(new DoubleDocValuesField("decayField", decayFactor));
-				
-				// BAD INSTRUCTIONS
-				// not using //doc.add(new DoublePoint("decayField", decayFactor)); // these don't work
-				// not using //doc.add(new StoredField("decayField", decayFactor)); // these don't work
-				
+            // Extract field CreationDate
+            try {
+                start = end + 1;
+                end = rawDocument.indexOf(',', start);
+                aux = rawDocument.substring(start, end);
+                Date creationDate;
+                creationDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(aux);
+                doc.add(new LongPoint("CreationDate", creationDate.getTime()));
 
-			} catch (ParseException e1) {
-				System.out.println("Error parsing date for document " + AnswerId);
-			}
-						
-			// Extract field ParentId
-			start = end + 1;
-			end = rawDocument.indexOf(',', start);
-			aux = rawDocument.substring(start, end);
-			Integer ParentId = Integer.decode(aux);
-			doc.add(new IntPoint("ParentId", ParentId));
+                // Add decay weight field
+                //System.out.println("??????????????????????????????????????????????");
+                LocalDate docDate = creationDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 
-			// Extract field Score
-			start = end + 1;
-			end = rawDocument.indexOf(',', start);
-			aux = rawDocument.substring(start, end);
-			Integer Score = Integer.decode(aux);
-			doc.add(new IntPoint("Score", Score));
+                Date docDate1 = Date.from(docDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+                Date now = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
+                long diff = now.getTime() - docDate1.getTime();
+                int totalDaysElapsed = (int) TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 
-			// Extract field Body
-			String body = rawDocument.substring(end + 1);
-			// I added this line below
-			doc.add(new TextField("Body", body, Field.Store.YES)); // Does this mean terms are also stored?
-			
-			// Add vector of terms for Body
-			// Create own FieldType to store Term Vectors
-		    
+                // Calculation of total elapsed number of days ends here
+                System.out.println(totalDaysElapsed);
+                //double decayFactor = 1.0 / Math.pow(2.0, totalDaysElapsed);
+                double decayFactor = 1.0 / Math.pow((1.0 + (1.0/(float)totalDaysElapsed)), (float)totalDaysElapsed);
 
-		// ====================================================
-		// Add the document to the index
-			if (idx.getConfig().getOpenMode() == IndexWriterConfig.OpenMode.CREATE) {
-				System.out.println("adding " + AnswerId);
-				// Don't know how to use this // float doc_boost = doc.getField(body).boost();
-				idx.addDocument(doc);
-			} else {
-				idx.updateDocument(new Term("AnswerId", AnswerId.toString()), doc);
-			}
-		} catch (IOException e) {
-			System.out.println("Error adding document " + AnswerId);
-		} catch (Exception e) {
-		System.out.println("Error parsing document " + AnswerId);
-		}
-	}
+                // DECAY
+                //doc.add(new DoubleDocValuesField("decayField", decayFactor));
 
-	// ====================================================
+
+            } catch (ParseException e1) {
+                System.out.println("Error parsing date for document " + AnswerId);
+            }
+
+            // Extract field ParentId
+            start = end + 1;
+            end = rawDocument.indexOf(',', start);
+            aux = rawDocument.substring(start, end);
+            Integer ParentId = Integer.decode(aux);
+            doc.add(new IntPoint("ParentId", ParentId));
+
+            // Extract field Score
+            start = end + 1;
+            end = rawDocument.indexOf(',', start);
+            aux = rawDocument.substring(start, end);
+            Integer Score = Integer.decode(aux);
+            doc.add(new IntPoint("Score", Score));
+
+            // Extract field Body
+            String body = rawDocument.substring(end + 1);
+            // I added this line below
+            doc.add(new TextField("Body", body, Field.Store.YES)); // Does this mean terms are also stored?
+
+            // Add vector of terms for Body
+            // Create own FieldType to store Term Vectors
+
+
+            // ====================================================
+            // Add the document to the index
+            if (idx.getConfig().getOpenMode() == IndexWriterConfig.OpenMode.CREATE) {
+                System.out.println("adding " + AnswerId);
+                // Don't know how to use this // float doc_boost = doc.getField(body).boost();
+                idx.addDocument(doc);
+            } else {
+                idx.updateDocument(new Term("AnswerId", AnswerId.toString()), doc);
+            }
+        } catch (IOException e) {
+            System.out.println("Error adding document " + AnswerId);
+        } catch (Exception e) {
+            System.out.println("Error parsing document " + AnswerId);
+        }
+    }
+
+    // ====================================================
     // Comment and refactor this method yourself
     public void indexSearch(Analyzer analyzer, Similarity similarity) {
 
@@ -245,7 +239,7 @@ public class Lab1_Baseline {
             reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
             // Get number of documents in index
             int nDocsInReader = reader.numDocs();
-            
+
             System.out.println("----------------------------------------------------------------------");
             System.out.println("# docs in index = "+nDocsInReader);
             System.out.println("----------------------------------------------------------------------");
@@ -260,7 +254,7 @@ public class Lab1_Baseline {
                 String simString = similarity.toString().replace("(", "_").replace(")", "_").replace(" ","_").replace("=", "").replaceAll("," , "_");
                 String filename = "./eval/"+simString+".txt";
                 File resultFile = new File(filename);
-                
+
                 //File resultFile = new File("./eval/classicSim.txt");
                 resultFile.delete();
                 resultFile.createNewFile();
@@ -278,20 +272,20 @@ public class Lab1_Baseline {
                     }
 
                     Query query;
-                    
-                    // JOANA DECAY
-                    ValueSource val = new DoubleFieldSource("decayField");
-                    FunctionQuery myFuncQuery = new FunctionQuery(val);
-                    
+
+                    //Decay
+                    //ValueSource val = new DoubleFieldSource("decayField");
+                    //FunctionQuery myFuncQuery = new FunctionQuery(val);
+
                     try {
                         query = parser.parse(line);
                     } catch (org.apache.lucene.queryparser.classic.ParseException e) {
                         System.out.println("Error parsing query string.");
                         continue;
                     }
-                    
-                    // JOANA DECAY
-                    query = new CustomScoreQuery(query, myFuncQuery);
+
+                    // DECAY
+//                    query = new CustomScoreQuery(query, myFuncQuery);
 
                     TopDocs results = searcher.search(query, 100);
                     ScoreDoc[] hits = results.scoreDocs;
@@ -307,7 +301,7 @@ public class Lab1_Baseline {
                         for (int j = 0; j < hits.length; j++) {
                             Document doc = searcher.doc(hits[j].doc);
                             long ntotterms = searcher.collectionStatistics("Body").sumTotalTermFreq();
-                            
+
                             //long numTotTermFreq = searcher.collectionStatistics("Body").sumTotalTermFreq();
                             //System.out.println("$$$"+"DocID="+hits[j].doc+"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
                             ////reader.getTermVector(hits[j].doc, "Body").iterator();
@@ -315,7 +309,7 @@ public class Lab1_Baseline {
                             //System.out.println(ntotterms);
                             //System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
                             //searcher.doc(hits[j].doc).getBinaryValue("Body") // not used
-                            System.out.println(searcher.explain(query, hits[j].doc));
+//                            System.out.println(searcher.explain(query, hits[j].doc));
                             String answer = doc.get("Body");
                             Integer AnswerId = doc.getField("AnswerId").numericValue().intValue();
                             out.println(questionID + "\t\t\tQ0\t\t\t"+ AnswerId + "\t\t\t" + (j+1) + "\t\t\t" + hits[j].score + "\t\t\trun-1");
--- src/main/java/Lab4_RetrievalModels.java
+++ src/main/java/Lab4_RetrievalModels.java
@@ -1,75 +1,53 @@
-package org.novasearch.tutorials.labs2018;
-
+import org.apache.lucene.search.similarities.Similarity;
 import java.util.List;
 import java.util.Arrays;
-
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.search.similarities.BM25Similarity;
-import org.apache.lucene.search.similarities.ClassicSimilarity;
 import org.apache.lucene.search.similarities.LMDirichletSimilarity;
 import org.apache.lucene.search.similarities.LMJelinekMercerSimilarity;
-import org.apache.lucene.search.similarities.LMSimilarity.CollectionModel;
-import org.apache.lucene.search.similarities.Similarity;
 
 public class Lab4_RetrievalModels extends Lab1_Baseline {
 
     public static void main(String[] args) {
 
-        // ===================================
-        // Default analyzer
-        Analyzer analyzer = new StandardAnalyzer();
 
+        //Analyzer analyzer = new StandardAnalyzer();
+        Lab2_Analyser analyzer = new Lab2_Analyser();
 
-		// ===================================
-        // Select the retrieval model function
-        // Similarity similarity = new ClassicSimilarity();
-        // Similarity similarity = new BM25Similarity();
-        // Similarity similarity = new LMDirichletSimilarity();
-        // Similarity similarity = new LMJelinekMercerSimilarity(lambda);
-        
-        // Para gerar os ficheiros com as metricas do LMJelinekMercer com 
-        /*for (int j = 1; j < 12; j++) {
-        	float lambda = (float)(j-1)/10;
-        	Similarity similarity = new LMJelinekMercerSimilarity(lambda);
-        	Lab1_Baseline baseline = new Lab1_Baseline();
-        	baseline.indexSearch(analyzer, similarity);
-        }*/
-        
-        /*List<Integer> mus = Arrays.asList(10, 100, 500, 1000, 5000);
-        
+        // Para gerar os ficheiros com as metricas do LMJelinekMercer com
+        for (int j = 1; j < 12; j++) {
+            float lambda = (float) (j - 1) / 10;
+            Similarity similarity = new LMJelinekMercerSimilarity(lambda);
+            Lab1_Baseline baseline = new Lab1_Baseline();
+//            baseline.openIndex(analyzer, similarity);   //creates an in-disk index
+//            baseline.indexDocuments();  //parses the documents and puts them in them index
+//            baseline.close();
+            baseline.indexSearch(analyzer, similarity);
+        }
+
+        List<Integer> mus = Arrays.asList(10, 100, 500, 1000, 5000);
+
         for (int miu : mus) {
-        	Similarity similarity = new LMDirichletSimilarity(miu);
-        	Lab1_Baseline baseline = new Lab1_Baseline();
-        	baseline.indexSearch(analyzer, similarity);
-        }*/
-        
-        /*List<Float> bs = Arrays.asList(0.0f, 0.25f, 0.5f, 0.75f, 1.0f);
-        List<Float> k1s = Arrays.asList(0.0f, 0.5f, 1.0f, 1.25f, 1.5f, 2.0f);
-        
-        for (float ib : bs) {
-        	for (float ik : k1s) {
-        		Similarity similarity = new BM25Similarity(ik,ib);
-        		Lab1_Baseline baseline = new Lab1_Baseline();
-        		baseline.indexSearch(analyzer, similarity);
-        	}
-        }*/
-        
-        // For testing purposes of the decay function
-        Similarity similarity = new BM25Similarity();
-		Lab1_Baseline baseline = new Lab1_Baseline();
-        
-        // ===================================
-        // The indexing process will use the provided analyzer and ranking function
-        // Lab1_Baseline baseline = new Lab1_Baseline();
-        // These lines should be commented since we only want to work on the search part
-        baseline.openIndex(analyzer, similarity);
-        baseline.indexDocuments();
-        baseline.close();
+            Similarity similarity = new LMDirichletSimilarity(miu);
+            Lab1_Baseline baseline = new Lab1_Baseline();
+//            baseline.openIndex(analyzer, similarity);   //creates an in-disk index
+//            baseline.indexDocuments();  //parses the documents and puts them in them index
+//            baseline.close();
+            baseline.indexSearch(analyzer, similarity);
+        }
 
-        // ===================================
-        // The search process will use the provided analyzer and ranking function
-        baseline.indexSearch(analyzer, similarity);
+        List<Float> bs = Arrays.asList(0.0f, 0.25f, 0.5f, 0.75f, 1.0f);
+        List<Float> k1s = Arrays.asList(0.0f, 0.5f, 1.0f, 1.25f, 1.5f, 2.0f);
+
+        for (float ib : bs) {
+            for (float ik : k1s) {
+                Similarity similarity = new BM25Similarity(ik, ib);
+                Lab1_Baseline baseline = new Lab1_Baseline();
+//                baseline.openIndex(analyzer, similarity);   //creates an in-disk index
+//                baseline.indexDocuments();  //parses the documents and puts them in them index
+//                baseline.close();
+                baseline.indexSearch(analyzer, similarity);
+            }
+        }
     }
 
 
